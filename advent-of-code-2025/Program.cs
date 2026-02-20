629313c9233ea6b695efca1e61cfcfe7f47349a8
Gwynneth Uhrhammer <gleece@gmail.com>
2026-02-20 09:08:20 -0800

    Problem 8 Part 2

--- advent-of-code-2025/Program.cs
+++ advent-of-code-2025/Program.cs
@@ -4,7 +4,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Problem8Part1();
+        Problem8Part2();
     }
 
     #region Problem 1
@@ -1189,5 +1189,102 @@ internal class Program
         Console.WriteLine($"RESULT: {result}");
     }
 
-        #endregion
+    private static void Problem8Part2()
+    {
+        var fileName = "../../../../input/input-8.txt";
+        var lines = File.ReadLines(fileName).ToArray();
+
+        if (lines.Count() == 0)
+        {
+            Console.WriteLine("Empty input");
+            return;
+        }
+
+        List<JunctionBox> junctionBoxes = new();
+
+        foreach (var line in lines)
+        {
+            var tokens = line.Split(",");
+            var box = new JunctionBox(
+                int.Parse(tokens[0]),
+                int.Parse(tokens[1]),
+                int.Parse(tokens[2]));
+
+            junctionBoxes.Add(box);
+        }
+
+        foreach (var position in junctionBoxes)
+        {
+            Console.WriteLine($"Junction box: {position.x}, {position.y}, {position.z}");
+        }
+
+        // Calculate distance between all pairs
+        List<JunctionBoxDistance> distances = new();
+        for (int a = 0; a < junctionBoxes.Count; a++)
+        {
+            var boxA = junctionBoxes[a];
+            for (int b = a + 1; b < junctionBoxes.Count; b++)
+            {
+                var boxB = junctionBoxes[b];
+                var distance = boxA.DistanceToOtherBox(boxB);
+                var boxDistance = new JunctionBoxDistance(boxA, boxB, distance);
+                distances.Add(boxDistance);
+            }
+        }
+
+        // Sort the distances
+        distances.Sort((a, b) => a.Distance.CompareTo(b.Distance));
+
+        // Make circuits of size 1 for each junction box
+        List<Circuit> circuits = new();
+        foreach (var box in junctionBoxes)
+        {
+            var circuit = new Circuit();
+            circuit.Boxes.Add(box);
+            box.ParentCircuit = circuit;
+            circuits.Add(circuit);
+        }
+
+        // Merge circuits for N closest pairs
+        for (int i = 0; i < distances.Count; i++)
+        {
+            var boxA = distances[i].BoxA;
+            var boxB = distances[i].BoxB;
+            Console.WriteLine($"MERGING, i = {i}, {boxA.DebugString} {boxB.DebugString}");
+
+            if (boxA.ParentCircuit == null || boxB.ParentCircuit == null)
+            {
+                Console.WriteLine($"Error: box has null parent circuit, this should not happen");
+                return;
+            }
+
+            if (boxA.ParentCircuit == boxB.ParentCircuit)
+            {
+                // Boxes are already connected
+                continue;
+            }
+
+            // Move all the boxes from Box B's circuit over into Box A's circuit
+            var otherCircuit = boxB.ParentCircuit;
+            var otherCircuitList = new List<JunctionBox>(boxB.ParentCircuit.Boxes);
+            foreach (var box in otherCircuitList)
+            {
+                boxA.ParentCircuit.Boxes.Add(box);
+                box.ParentCircuit = boxA.ParentCircuit;
+            }
+            circuits.Remove(otherCircuit);
+
+            if (circuits.Count == 1)
+            {
+                Console.WriteLine($"DONE! Last connection made: {boxA.DebugString}, {boxB.DebugString}");
+                long result = (long)boxA.x * (long)boxB.x;
+                Console.WriteLine($"Result: {result}");
+                break;
+            }
+
+            Console.WriteLine("--------------------------------------");
+        }
     }
+
+    #endregion
+}
