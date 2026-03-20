9765e3c88476912d08bc059d04e2e84747359383
Gwynneth Uhrhammer <gleece@gmail.com>
2026-03-20 10:37:36 -0700

    Problem 1 Part 1 in C++

--- project-c-plus-plus/project-c-plus-plus.cpp
+++ project-c-plus-plus/project-c-plus-plus.cpp
@@ -1,20 +1,68 @@
 // project-c-plus-plus.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <fstream>
 #include <iostream>
+#include <string>
+
+void Problem1Part1()
+{
+    std::string filePath = "Input/input-1.txt";
+    std::ifstream file (filePath);
+    if (!file)
+    {
+        std::cerr << "Could not open file " << filePath << '\n';
+        return;
+    }
+    
+    int dialPosition = 50;
+    int dialSize = 100;
+    int zeroCounter = 0;
+    
+    std::string line;
+    while (std::getline(file, line))
+    {
+        if (line.empty())
+        {
+            std::cerr << "Found empty line in" << filePath << ", skipping" << '\n';
+            continue;
+        }
+        std::cout << line << '\n';
+        
+        // First character of the line indicates direction
+        char direction = line[0];
+        
+        // Remaining characters are the rotation distance
+        std::string distanceString = line.substr(1);
+        int distance = atoi(distanceString.c_str());
+        
+        if (direction == 'L')
+        {
+            dialPosition -= distance;
+            while (dialPosition < 0)
+            {
+                dialPosition += dialSize;
+            }
+            dialPosition = dialPosition % dialSize;
+        }
+        else if (direction == 'R')
+        {
+            dialPosition += distance;
+            dialPosition = dialPosition % dialSize;
+        }
+        
+        if (dialPosition == 0)
+        {
+            zeroCounter++;
+        }
+    }
+    
+    std::cout << "Zero count: " << zeroCounter << '\n';
+}
 
 int main()
 {
     std::cout << "Hello World!\n";
+    
+    Problem1Part1();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
